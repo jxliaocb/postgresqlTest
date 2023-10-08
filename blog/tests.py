baf1f8dde9a818cee4c2a6aefc0f94cf9f3f0e85
Changbing Liao <dennisliao.everest@gmail.com>
2023-10-09 06:58:29 +1100

    modify the test file for blog post test

--- blog/tests.py
+++ blog/tests.py
@@ -2,20 +2,21 @@
 from django.test import TestCase
 from .models import Post
 
-# class PostModelTestCase(TestCase):
-#     def test_create_post(self):
-#         post = Post.objects.create(title="Test Title", body="Test Content")
-#         self.assertEqual(Post.objects.count(), 1)
-#         self.assertEqual(post.title, "Test Title")
-# from django.urls import reverse
+class PostModelTestCase(TestCase):
+    def test_create_post(self):
+        post = Post.objects.create(title="Test Title", body="Test Content")
+        self.assertEqual(Post.objects.count(), 1)
+        self.assertEqual(post.title, "Test Title")
 
-# class PostListViewTestCase(TestCase):
-#     def setUp(self):
-#         self.url = reverse('post_list')
-#         Post.objects.create(title="Test Title", body="Test Content")
+from django.urls import reverse
 
-#     def test_view_uses_correct_template(self):
-#         response = self.client.get(self.url)
-#         self.assertEqual(response.status_code, 200)
-#         self.assertTemplateUsed(response, 'blog/post_list.html')
-#         self.assertContains(response, "Test Title")
+class PostListViewTestCase(TestCase):
+    def setUp(self):
+        self.url = reverse('frontpage')
+        Post.objects.create(title="Test Title", slug='1', intro='Test case practice', body="Test Content")
+
+    def test_view_uses_correct_template(self):
+        response = self.client.get(self.url)
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'blog/frontpage.html')
+        self.assertContains(response, "Test Title")
